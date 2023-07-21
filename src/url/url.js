63cb9408e210a2549c0a8ed640127bc997fcdf21
ritesh <chaudharyritesh947@gmail.com>
2023-07-21 09:57:39 +0530

    url file

---
+++ src/url/url.js
@@ -0,0 +1,4 @@
+const domain = 'http://localhost:4000'
+exports.GET_ENQUIRIES = `${domain}/api/enquiry`;
+exports.LOGIN = `${domain}/api/login`;
+exports.SAVE_TRAVEL = '${domain}/api/enquiry/create';
